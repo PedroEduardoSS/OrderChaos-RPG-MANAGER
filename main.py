b55bf1c087b5d558a301e730f0b17d14eaf5e745
PedroEduardoSS <pedroeduardossousa@gmail.com>
2021-12-24 14:03:23 -0300

    refactored the code

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import dearpygui.dearpygui as dpg
-from master_window import *
-from player_window import *
+from views.master_window import *
+from views.player_window import *
 
 dpg.create_context()
 dpg.create_viewport(title='RPG - Manager', width=1200, height=720)
