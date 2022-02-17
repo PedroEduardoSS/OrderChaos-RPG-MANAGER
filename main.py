6ff623835fd96bde6fc30f9ff09b780c6070db93
PedroEd <pedroeduardossousa@gmail.com>
2022-02-17 10:17:36 -0300

    Updated main window

--- main.py
+++ main.py
@@ -7,12 +7,35 @@ dpg.create_context()
 dpg.create_viewport(title='RPG - Manager', width=1200, height=720, small_icon="icon.ico", large_icon="icon.ico")
 dpg.setup_dearpygui()
 
+def confirmar_1():
+    master_window()
+
+def confirmar_2():
+    player_window()
+
+def perfil():
+    width1, height1, channels1, data1 = dpg.load_image("imgs/mestre.png")
+    width2, height2, channels2, data2 = dpg.load_image("imgs/jogador.png")
+
+    with dpg.texture_registry():
+        dpg.add_static_texture(width1, height1, data1, tag="mestre_img")
+        dpg.add_static_texture(width2, height2, data2, tag="jogador_img")
+
+    with dpg.window(label="Início", width=400, height=250, pos=(400,100), collapsed=False):
+        with dpg.group(width=190, horizontal=True):
+            with dpg.group():
+                dpg.add_image("mestre_img", width=150, height=150)
+                dpg.add_text("Eu sou Mestre")
+                dpg.add_button(tag="C_btn1", label="Confirmar", width=100, height=30, callback=confirmar_1)
+            with dpg.group():
+                dpg.add_image("jogador_img", width=150, height=150)
+                dpg.add_text("Eu sou Jogador")
+                dpg.add_button(tag="C_btn2", label="Confirmar", width=100, height=30, callback=confirmar_2)
+
 with dpg.window(tag="Primary Window"):
     with dpg.menu_bar():
         dpg.add_menu_item(label="Regras Gerais", callback=lambda: rules_window())
-        with dpg.menu(label="Mestre/Jogador"):
-            dpg.add_menu_item(label="Covil do Mestre", callback=lambda: master_window())
-            dpg.add_menu_item(label="Ficha do Jogador", callback=lambda: player_window())
+        dpg.add_menu_item(label="Mestre/Jogador", callback=lambda: perfil())
 
 if __name__ == "__main__":
     dpg.show_viewport()
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from cx_Freeze import setup, Executable
 
 build_exe_options = {"packages": ["dearpygui", "json"],
 "excludes": ["tkinter", "html", "email"],
-"include_files": ["models", "__pycache__", "icon.ico"]}
+"include_files": ["models", "imgs", "icon.ico"]}
 
 # base="Win32GUI" should be used only for Windows GUI app
 base = None
@@ -11,8 +11,8 @@ if sys.platform == "win32":
     base = "Win32GUI"
 
 setup(
-    name = "RPG - MANAGER",
-    version = "0.1",
+    name = "OrderChaos(RPG - MANAGER)",
+    version = "1.1",
     description = "Gererenciador de Role-Playing Game",
     options = {"build_exe": build_exe_options},
     executables = [Executable("main.py", icon="icon.ico", base=base)]
